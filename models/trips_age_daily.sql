a22d800dbb4b331ce53a25214841788b3ad05aa8
BlagoSvet-tech <svetlar03@gmail.com>
2026-03-01 12:46:30 +0200

    Create trips_users and correct trips_ade_daily and trips_ade_daily_stat

--- models/trips_age_daily.sql
+++ models/trips_age_daily.sql
@@ -1,8 +1,7 @@
-select extract(year from t.started_at) - extract(year from u.birth_date) as ages,
-		   t.started_at::date as started_date,
-		   count(distinct t.id) as cnt_trips,
-           sum(price_rub) as revenue_rub
-from {{source("scooters_raw", "users")}} u 
-join {{ref("trips_prep")}} t on u.id = t.user_id 
-group by extract(year from t.started_at) - extract(year from u.birth_date),
-		t.started_at::date
+select age,
+	   date,
+	   count(distinct id) as cnt_trips,
+       sum(price_rub) as revenue_rub
+from {{ref("trips_users")}}
+group by age,
+		 date
