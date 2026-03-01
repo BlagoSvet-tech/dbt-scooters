3503330a0ee916112f181f645e0bdea581c76bf5
BlagoSvet-tech <svetlar03@gmail.com>
2026-03-01 14:27:59 +0200

    Change events_clean to incremental table

---
+++ models/events_clean.sql
@@ -0,0 +1,8 @@
+select distinct user_id, "timestamp", type_id
+from {{source("scooters_raw", "events")}}
+{% if is_incremental() %}
+    where "timestamp" > (select max("timestamp") from {{ this }})
+    order by "timestamp" 
+{% else %}
+    where "timestamp" < '2023-08-01'::date 
+{% endif %}
